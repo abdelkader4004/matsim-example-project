7c3dc912330ff30c5dcffc095c5af12bf8047be5
abd <aek.benzohra@gmail.com>
2022-12-03 10:32:52 +0100

    Tests

---
+++ src/test/java/org/matsim/project/RunMatsimTestsTest.java
@@ -0,0 +1,14 @@
+package org.matsim.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunMatsimTestsTest {
+@Test
+    public void testSomeThing(){
+
+Assert.assertEquals(10,8+2);
+}
+}
