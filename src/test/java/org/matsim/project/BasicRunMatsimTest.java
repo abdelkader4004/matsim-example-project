2393849715850cc844fe57a58b234f813836a1ce
abd <aek.benzohra@gmail.com>
2022-12-08 16:42:24 +0100

    Tests

---
+++ src/test/java/org/matsim/project/BasicRunMatsimTest.java
@@ -0,0 +1,14 @@
+package org.matsim.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicRunMatsimTest {
+@Test
+public void tesTtheCode(){
+    Assert.assertEquals(3,2+1);
+
+}
+}
---
+++ src/test/java/org/matsim/project/RunMatsim2Test.java
@@ -0,0 +1,34 @@
+package org.matsim.project;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.testcases.MatsimTestUtils;
+
+
+
+public class RunMatsim2Test {
+
+    @Rule
+    public MatsimTestUtils utils =new MatsimTestUtils();
+
+
+    @Test
+    public void testSomeThing(){
+//    System.out.println("1 "+ utils.getOutputDirectory());
+//    System.out.println("2 "+ utils.getInputDirectory());
+//Assert.assertEquals(10,8+2);
+
+//
+//        Config config = ConfigUtils.createConfig() ;
+//        config.network().setInputFile(utils.getClassInputDirectory() + "/network.xml.gz");
+//        config.plans().setInputFile(utils.getInputDirectory()+ "/plans.xml.gz");
+//        config.controler().setOutputDirectory( utils.getOutputDirectory() );
+//        var scenario= ScenarioUtils.loadScenario(config);
+//        Controler controller=new Controler(scenario);
+//        controller.run();
+    }
+}
